6cd4d3caa98ff9822c37c8cc3d4f5a28629358d9
G184892021 <86590276+G184892021@users.noreply.github.com>
2021-07-26 16:47:33 +0900

    更新 pxt.json, main.ts, main.py, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,57 @@
 scene.setBackgroundColor(8)
+let spacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . 6 . . . . . . . . . . . . . .
+    . . 6 6 d . . . . . . . . . . .
+    . . d 6 6 6 9 9 . . . . . . . .
+    4 4 2 6 6 6 6 6 d d d d d . . .
+    . . d d d 6 6 6 6 d d . . . . .
+    . d d d 6 6 . . . . . . . . . .
+    d . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Player)
+info.setLife(3)
+spacePlane.setStayInScreen(true)
+controller.moveSprite(spacePlane, 200, 200)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    let missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . 2 2 . . . . . . . . . . . .
+            4 4 d d d d d 2 . . . . . . . .
+            . . 2 2 . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, spacePlane, 200, 0)
+})
+game.onUpdateInterval(500, function on_update() {
+    let bogy = sprites.create(assets.image`bogy`, SpriteKind.Enemy)
+    bogy.setVelocity(-100, randint(-30, 30))
+    bogy.y = randint(0, scene.screenHeight())
+    bogy.left = scene.screenWidth()
+    bogy.setFlag(SpriteFlag.AutoDestroy, true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_hit(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_crash(spirte: Sprite, othersprite: Sprite) {
+    othersprite.destroy()
+    info.changeLifeBy(-1)
+})
